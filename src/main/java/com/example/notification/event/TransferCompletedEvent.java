e9338d4cea5688203a0709397be7363481a289b9
Кузьмин Александр Владимирович <al.kuzmin@korona.net>
2025-12-15 12:47:33 +0700

    Initial commit

---
+++ src/main/java/com/example/notification/event/TransferCompletedEvent.java
@@ -0,0 +1,22 @@
+package com.example.notification.event;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class TransferCompletedEvent {
+
+    private UUID transferId;
+
+    private UUID fromAccountId;
+
+    private UUID toAccountId;
+
+    private BigDecimal amount;
+}
